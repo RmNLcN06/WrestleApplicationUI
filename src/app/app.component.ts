6757704c497fcc38e76b7da024bdcca7ba4edadd
RmNLcN06 <romain.luciano@orange.fr>
2024-07-06 13:18:59 +0200

    Add getContinents method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Continent } from './models/continent';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'WrestleApplicationUI';
+  http = inject(HttpClient);
+
+  continents = this.getContinents();
+
+  private getContinents(): Observable<Continent[]>
+  {
+    return this.http.get<Continent[]>('https://localhost:7111/api/continents');
+  }
 }
