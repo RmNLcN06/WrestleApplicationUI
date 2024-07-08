abeb25699479483e15379b302d10ccdff3d7dec9
RmNLcN06 <romain.luciano@orange.fr>
2024-07-08 18:58:23 +0200

    Create components folder & files

--- src/app/components/continent/continent.component.ts
+++ src/app/components/continent/continent.component.ts
@@ -1,19 +1,18 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Continent } from './models/continent';
+import { Continent } from '../../models/continent';
 import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
-import { ContinentComponent } from './components/continent/continent.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-continent',
   standalone: true,
   imports: [RouterOutlet, HttpClientModule, AsyncPipe, ContinentComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './continent.component.html',
+  styleUrl: './continent.component.css'
 })
-export class AppComponent {
+export class ContinentComponent {
   title = 'WrestleApplicationUI';
   http = inject(HttpClient);
 
@@ -24,3 +23,7 @@ export class AppComponent {
     return this.http.get<Continent[]>('https://localhost:7111/api/continents');
   }
 }
+
+
+
+
